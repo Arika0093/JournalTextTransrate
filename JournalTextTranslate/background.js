614f9f2fae7c17e8304cb5748bc7df5b75466560
Arika <Delete0093@Gmail.com>
2018-08-17 13:02:22 +0900

    first

---
+++ JournalTextTranslate/background.js
@@ -0,0 +1,25 @@
+﻿chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+        title: 'Translate Selected-Text on NewTab',
+        id: 'contransmenu',
+        contexts: ['selection'],
+    });
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId === "contransmenu") {
+		var TransText = info.selectionText;
+		var TextNoIndention = TransText
+			.replace(/-\r/g, "")
+			.replace(/-\n/g, "")
+			.replace(/\r-/g, "")
+			.replace(/\n-/g, "")
+			.replace(/\r/g, "")
+			.replace(/\n/g, " ");
+		var TextIndentAfterDot = TextNoIndention.replace(/\. /g, ".\r\n");
+		var TransTextEncoded = encodeURIComponent(TextIndentAfterDot);
+		chrome.tabs.create({
+			url: "https://translate.google.co.jp/#en/ja/" + TransTextEncoded
+		});
+    }
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+# JournalTextTranslate
+選択した範囲の文字列をChrome翻訳(En→Ja)にぶん投げるだけの拡張機能
+複数行の場合はうまいこと分離してくれます
+
+## 導入方法
+- [Releaseページ](https://github.com/Arika0093/JournalTextTransrate/release)へ移動
+- source code (zip) を選択して保存、解凍
+- chrome拡張機能のページへ移動して、デベロッパーモードをONに
+- `JournalTextTranslate.crx`をchromeにドラッグドロップ
+- 導入完了
+
